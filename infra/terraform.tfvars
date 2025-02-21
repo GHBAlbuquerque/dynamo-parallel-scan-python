64548399a061237a4853690f57415cb5480a241a
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-02-21 12:19:39 -0300

    infra: creates lambda infra classes

---
+++ infra/terraform.tfvars
@@ -0,0 +1,4 @@
+project_name = "dynamo_parallel_scan_python"
+region       = "us-east-1"
+profile      = "default"
+role_arn = "arn:aws:iam::xxxxx:role/xxxx"
---
+++ infra/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+  default = "project_name"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "profile" {
+  default = "default"
+}
+
+variable "role_arn" {
+  default = "role"
+}
