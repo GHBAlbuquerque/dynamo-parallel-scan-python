58013930cbf9fd49e77bc2ed2ad0495125a92006
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-02-24 10:24:50 -0300

    feature: preenche atributos do handler

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,7 +1,7 @@
 import asyncio
 
-from app.src.createEvent.event_producer import EventProducer
-from app.src.tableScan.scan_by_segment import TableScanProcessor
+from src.createEvent.event_producer import EventProducer
+from src.tableScan.scan_by_segment import TableScanProcessor
 
 def lambda_handler(event: dict, context): 
 
@@ -11,14 +11,15 @@ def lambda_handler(event: dict, context):
 
     result = loop.run_until_complete(
             table_scan_processor.scan_dynamodb_with_segments(
-                    'my-table',
-                    'my-attribute',
-                    'my-sk-filter',
+                    'TestTable', #table-name
+                    'PK', #my-pk-filter
+                    'SK', #my-sk-filter
                     event["current_segment"],
                     event["total_segments"],
                     event["limit_of_rows"]
             )
     )
+
     return _build_output(result)
 
 def _build_output(result):
